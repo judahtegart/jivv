f045334d623bd9083739213a002e9711e35eb8ee
Judah <judahtegart@gmail.com>
2023-01-24 23:29:56 -0800

    Added mute and unmute features
    Added the ability to mute and unmute members using the @Muted role.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+console.log(`Logged in as ${client.user.tag}!`);
 });
 
 function banCommand(message, args) {
@@ -22,13 +22,71 @@ function kickCommand(message, args) {
     .catch(error => message.reply(`Sorry, an error occured. ${error}`));
 }
 
+function muteCommand(message, args) {
+    const member = message.guild.members.cache.find(member => member.user.username === args[0] || member.id === args[0]);
+    if(!member) return message.reply("User not found");
+    let muteRole = message.guild.roles.cache.find(role => role.name === "Muted");
+    if(!muteRole){
+        try {
+            muteRole = message.guild.roles.create({
+                data: {
+                  name: "Muted",
+                  color: "#000000",
+                  permissions: []
+                }
+              })
+            message.guild.channels.cache.forEach(channel => {
+                channel.updateOverwrite(muteRole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false,
+                    SPEAK: false
+                });
+            });
+        } catch (error) {
+            console.log(error.stack);
+        }
+    }
+    if(member.roles.cache.has(muteRole.id)){
+        return message.reply("This user is already muted!");
+    }else{
+        member.roles.add(muteRole).then(() => {
+            message.reply(`${member.user.username} has been muted.`);
+        }).catch(error => {
+            message.reply(`Sorry, an error occured. ${error}`);
+        });
+    }
+}
+
+function unmuteCommand(message, args) {
+    const member = message.guild.members.cache.find(member => member.user.username === args[0] || member.id === args[0]);
+    if(!member) return message.reply("User not found");
+    let muteRole = message.guild.roles.cache.find(role => role.name === "Muted");
+    if(!muteRole) return message.reply("Muted role not found");
+    if(!member.roles.cache.has(muteRole.id)){
+        return message.reply("This user is not muted!");
+    }else{
+        member.roles.remove(muteRole).then(() => {
+            message.reply(`${member.user.username} has been unmuted.`);
+        }).catch(error => {
+            message.reply(`Sorry, an error occured. ${error}`);
+        });
+    }
+}
+      
 client.on('message', message => {
     if (message.content.startsWith("/ban")) {
         banCommand(message, message.content.split(" ").slice(1));
     }
-    if (message.content.startsWith("/Kick")) {
+    if (message.content.startsWith("/kick")) {
         kickCommand(message, message.content.split(" ").slice(1));
     }
+    if (message.content.startsWith("/mute")) {
+        muteCommand(message, message.content.split(" ").slice(1));
+    }
+     if (message.content.startsWith("/unmute")) {
+        unmuteCommand(message, message.content.split(" ").slice(1));
+    }
 });
+    
 
 client.login(process.env.TOKEN);
